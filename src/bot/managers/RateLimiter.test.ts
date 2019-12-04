c7d30b5ab51234d30030546235997f02adc1ff68
John Daniels <johnsdaniels@gmail.com>
2019-12-04 01:01:51 -0500

    Fix styles for linter.

--- src/bot/managers/RateLimiter.test.ts
+++ src/bot/managers/RateLimiter.test.ts
@@ -1,48 +1,46 @@
 import RateLimiter from './RateLimiter';
 
-
 test('Basic Rate Limiting', () => {
-    let currentTime = 0;
-    function timeMock() {
-        return currentTime;
-    }
-    let limiter = new RateLimiter(1000, 1, 2, timeMock);
-    expect(limiter.tryRemoveToken("bob")).toBe(true);
-    expect(limiter.tryRemoveToken("bob")).toBe(true);
-    expect(limiter.tryRemoveToken("bob")).toBe(false);
-    currentTime += 1500;
-    expect(limiter.tryRemoveToken("bob")).toBe(true);
-    expect(limiter.tryRemoveToken("bob")).toBe(false);
-    currentTime += 1500;
-    expect(limiter.tryRemoveToken("bob")).toBe(true);
-    expect(limiter.tryRemoveToken("bob")).toBe(true);
-    expect(limiter.tryRemoveToken("bob")).toBe(false);
+	let currentTime: number = 0;
+	function timeMock(): number {
+		return currentTime;
+	}
+	const limiter: RateLimiter = new RateLimiter(1000, 1, 2, timeMock);
+	expect(limiter.tryRemoveToken('bob')).toBe(true);
+	expect(limiter.tryRemoveToken('bob')).toBe(true);
+	expect(limiter.tryRemoveToken('bob')).toBe(false);
+	currentTime += 1500;
+	expect(limiter.tryRemoveToken('bob')).toBe(true);
+	expect(limiter.tryRemoveToken('bob')).toBe(false);
+	currentTime += 1500;
+	expect(limiter.tryRemoveToken('bob')).toBe(true);
+	expect(limiter.tryRemoveToken('bob')).toBe(true);
+	expect(limiter.tryRemoveToken('bob')).toBe(false);
 });
 
-
 test('Per User rate Limiting', () => {
-    let currentTime = 0;
-    function timeMock() {
-        return currentTime;
-    }
-    let limiter = new RateLimiter(1000, 1, 2, timeMock);
-    expect(limiter.tryRemoveTokens("bob", 2)).toBe(true);
-    expect(limiter.tryRemoveToken("bob")).toBe(false);
-    expect(limiter.tryRemoveTokens("alice", 1)).toBe(true);
-    currentTime += 1500;
-    expect(limiter.tryRemoveTokens("alice", 2)).toBe(true);
-    expect(limiter.tryRemoveTokens("alice", 1)).toBe(false);
+	let currentTime: number = 0;
+	function timeMock(): number {
+		return currentTime;
+	}
+	const limiter: RateLimiter = new RateLimiter(1000, 1, 2, timeMock);
+	expect(limiter.tryRemoveTokens('bob', 2)).toBe(true);
+	expect(limiter.tryRemoveToken('bob')).toBe(false);
+	expect(limiter.tryRemoveTokens('alice', 1)).toBe(true);
+	currentTime += 1500;
+	expect(limiter.tryRemoveTokens('alice', 2)).toBe(true);
+	expect(limiter.tryRemoveTokens('alice', 1)).toBe(false);
 });
 
 test('Bursting', () => {
-    let currentTime = 0;
-    function timeMock() {
-        return currentTime;
-    }
-    let limiter = new RateLimiter(1000, 1, 10000, timeMock);
-    expect(limiter.tryRemoveTokens("bob", 10000)).toBe(true);
-    currentTime += 10000;
-    // We can burst and still earn back slowly
-    expect(limiter.tryRemoveTokens("bob", 13)).toBe(false);
-    expect(limiter.tryRemoveTokens("bob", 10)).toBe(true);
+	let currentTime: number = 0;
+	function timeMock(): number {
+		return currentTime;
+	}
+	const limiter: RateLimiter = new RateLimiter(1000, 1, 10000, timeMock);
+	expect(limiter.tryRemoveTokens('bob', 10000)).toBe(true);
+	currentTime += 10000;
+	// We can burst and still earn back slowly
+	expect(limiter.tryRemoveTokens('bob', 13)).toBe(false);
+	expect(limiter.tryRemoveTokens('bob', 10)).toBe(true);
 });
