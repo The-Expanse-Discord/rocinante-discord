c8d62ceb525e4111e4d69f9a093f654957989eb4
John Daniels <johnsdaniels@gmail.com>
2021-01-02 21:37:34 -0500

    Move to using json5 for config so we can have comments.

--- src/Infrastructure/Client/Config.ts
+++ src/Infrastructure/Client/Config.ts
@@ -1,11 +1,12 @@
 import * as fs from 'fs';
 import * as yargs from 'yargs';
+import JSON5 from 'json5';
 import Configuration from '../System/Configuration';
 
 /** @internal */
 const rawConfig: string = fs.readFileSync(yargs.argv.config as string, 'utf8');
 
 /** @internal */
-const configDiscordClient: Configuration = new Configuration(JSON.parse(rawConfig));
+const configDiscordClient: Configuration = new Configuration(JSON5.parse(rawConfig));
 
 export { configDiscordClient };
