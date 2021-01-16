10fcf2d0f12cc1f1d683324b711b7657d8cb73fa
John Daniels <johnsdaniels@gmail.com>
2021-01-15 19:33:25 -0500

    Use raw events (#28)
    * Lowercase roles/emoji to make them match.
    
    They weren't matching if there was a lowercase word in the role
    due to using title case.
    
    * Update some libraries.
    
    * Update the max function length to 100.
    
    Honestly I'm not sure how much max function length makes sense
    in javascript where having a container function for a bunch of
    other functions (which is happening here) is commom.
    
    * Switch to handling reactions using raw events.
    
    discord.js' handling of reactions is brittle, because it requires
    all the structures it passes along to already be cached. This
    switches us over to using the raw messages and fetching from
    and populating caches by ourselves.

--- src/Infrastructure/Handlers/EventHandler.ts
+++ src/Infrastructure/Handlers/EventHandler.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { DiscordEvent } from '../Enums/System';
-import { Message, MessageReaction, PartialUser, User } from 'discord.js';
+import { Message, MessageReaction, User, TextChannel } from 'discord.js';
 import Protomolecule from '../Client/Protomolecule';
 
 /**
@@ -28,26 +28,59 @@ export class EventHandler {
 			await this.client.commandHandler!.processCommand(message);
 		});
 
-		this.client.on(DiscordEvent.AddReaction,
-			async(reaction: MessageReaction, user: User | PartialUser): Promise<void> => {
-				if (user.bot || !this.client.isReady()) {
-					return;
-				}
-
-				await this.client.roleManager.addRole(reaction, user);
-			});
-
-		this.client.on(DiscordEvent.RemoveReaction,
-			async(reaction: MessageReaction, user: User | PartialUser): Promise<void> => {
-				if (user.bot || !this.client.isReady()) {
-					return;
-				}
-
-				await this.client.roleManager.removeRole(reaction, user);
-			});
-
 		this.client.on(DiscordEvent.Disconnect, () => {
 			process.exit(100);
+		});
+
+		this.client.on('raw', async packet => {
+			// We implement this as a raw handler because the add/remove reactions are brittle and can't handle not having users/messages cached.
+			if (!this.client.isReady()) {
+				return;
+			}
+			console.log(`received message of type ${ packet.t }`);
+			if ([ 'MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE' ].includes(packet.t)) {
+				// Grab the channel to check the message from
+				const channel : TextChannel = this.client.channels.cache.get(packet.d.channel_id) as TextChannel;
+
+				// If we don't have the user, fetch it
+				if (!this.client.users.cache.has(packet.d.user_id)) {
+					await this.client.users.fetch(packet.d.user_id);
+				}
+				const user : User | undefined = this.client.users.cache.get(packet.d.user_id);
+				if (!user) {
+					console.log('No user fetched for reaction');
+					return;
+				}
+
+				// If we don't have the message, fetch it
+				if (!channel.messages.cache.has(packet.d.message_id)) {
+					await channel.messages.fetch(packet.d.message_id);
+				}
+				const message : Message | undefined = channel.messages.cache.get(packet.d.message_id);
+				if (!message) {
+					console.log('No message fetched for reaction');
+					return;
+				}
+				// Emojis can have identifiers of name:id format, so we have to account for that case as well
+				const emoji : string = packet.d.emoji.id ?
+					packet.d.emoji.id :
+					packet.d.emoji.name;
+				const reaction : MessageReaction | undefined = message.reactions.cache.get(emoji);
+				if (!reaction) {
+					return;
+				}
+				if (user.bot) {
+					return;
+				}
+
+				if (packet.t === 'MESSAGE_REACTION_ADD') {
+					await this.client.roleManager.addRole(reaction, user);
+				}
+				if (packet.t === 'MESSAGE_REACTION_REMOVE') {
+					await this.client.roleManager.removeRole(reaction, user);
+				}
+				return;
+			}
 		});
 	}
 }
